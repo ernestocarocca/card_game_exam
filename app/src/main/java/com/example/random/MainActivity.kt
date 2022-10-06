acfd13607418a569319904dc8fa6b5e8dc883d05
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2022-10-06 11:21:15 +0200

    jag har byt sista sidan bild

--- app/src/main/java/com/example/random/MainActivity.kt
+++ app/src/main/java/com/example/random/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val startIntent = Intent(this, MainActivityfirstside::class.java)
         startActivity(startIntent)
 
